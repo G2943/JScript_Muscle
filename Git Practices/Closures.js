6555287c35980b04f3b276e86c16428fad354903
Vaibhav Narayan Gole <vaibhavgole88@gmail.com>
2023-09-25 20:06:10 +0530

    folder name update

---
+++ Git Practices/Closures.js
@@ -0,0 +1,13 @@
+function closureFunc(){
+    let a = "Hello";
+    function insideClosure(){
+        // a = "inClosure"
+        console.log("Func Inside Closure : "+a);
+    }
+    a = "after Function"
+    insideClosure()
+}
+
+let x = closureFunc();
+
+console.log(x);
